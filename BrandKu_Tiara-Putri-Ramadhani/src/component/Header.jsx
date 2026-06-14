27949203946c4e0fde9eb0705e8ddcd0cce93cf9
Tiara Putri Ramadhani <tiarapramadhan@gmail.com>
2026-06-14 13:54:23 +0700

    feat: setup Vite React project and configure Tailwind CSS

---
+++ BrandKu_Tiara-Putri-Ramadhani/src/component/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => (
+    <header class="bg-white shadow-sm">
+    <div class="max-w-6xl mx-auto flex justify-between items-center p-4">
+      <h1 class="text-blue-600 font-bold text-xl">BrandKu</h1>
+      <nav class="space-x-6">
+        <a href="#" class="text-gray-700 hover:text-blue-500">Beranda</a>
+        <a href="#" class="text-gray-700 hover:text-blue-500">Fitur</a>
+        <a href="#" class="text-gray-700 hover:text-blue-500">Harga</a>
+      </nav>
+    </div>
+  </header>
+)
+
+export default Header;
